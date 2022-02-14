1159f1bc9668b49d6236f7a77c3b7056ef4e3f76
iam-eugene <mai77eugene@gmail.com>
2022-02-14 21:15:52 +0900

    🚧 Implement toggling order-form-modal

---
+++ js/order-form-modal.js
@@ -0,0 +1,19 @@
+const orderCta = document.querySelector('.order-cta')
+const [orderCtaBookmarkBtn, orderCtaBuyBtn] = orderCta.children
+// const orderCtaBookmarkBtn = orderCta.children[0]
+// const orderCtaBuyBtn = orderCta.children[1]
+
+const orderModal = document.querySelector('.order-form-modal')
+const orderModalOverlay = document.querySelector('.overlay')
+
+function openOrderModal() {
+  orderModal.classList.add('is-open')
+  orderModalOverlay.classList.add('is-active')
+}
+orderCtaBuyBtn.addEventListener('click', openOrderModal)
+
+function closeOrderModal() {
+  orderModal.classList.remove('is-open')
+  orderModalOverlay.classList.remove('is-active')
+}
+orderModalOverlay.addEventListener('click', closeOrderModal)
