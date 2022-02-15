23f3288049ccd36d0151321b4813a8f01fe6f9bd
iam-eugene <mai77eugene@gmail.com>
2022-02-15 18:02:28 +0900

    🚧 Implement toggling bookmark button

--- js/order-form-modal.js
+++ js/order-form-modal.js
@@ -1,7 +1,5 @@
 const orderCta = document.querySelector('.order-cta')
 const [orderCtaBookmarkBtn, orderCtaBuyBtn] = orderCta.children
-// const orderCtaBookmarkBtn = orderCta.children[0]
-// const orderCtaBuyBtn = orderCta.children[1]
 
 const orderModal = document.querySelector('.order-form-modal')
 const orderModalOverlay = document.querySelector('.overlay')
@@ -17,3 +15,22 @@ function closeOrderModal() {
   orderModalOverlay.classList.remove('is-active')
 }
 orderModalOverlay.addEventListener('click', closeOrderModal)
+
+function toggleOrderCtaBookmark() {
+  const [icon, countSpan] = this.children
+  const count = Number(countSpan.innerHTML.replace(',', ''))
+
+  let newCount = count
+  if (this.classList.contains('is-active')) {
+    icon.setAttribute('class', 'ic-bookmark')
+    newCount = newCount - 1
+  } else {
+    icon.setAttribute('class', 'ic-bookmark-filled')
+    newCount = newCount + 1
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString()
+  countSpan.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`)
+  this.classList.toggle('is-active')
+}
+orderCtaBookmarkBtn.addEventListener('click', toggleOrderCtaBookmark)
