b5b70ec4400f1580c91ecc010859c48124540bfd
iam-eugene <mai77eugene@gmail.com>
2022-02-17 17:20:38 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,11 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearch.querySelector('ol')
+
+const deleteAllBtn = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
 
 function closeGnbSearchHistory(e) {
   if (!gnbSearch.contains(e.target)) {
@@ -10,9 +15,19 @@ function closeGnbSearchHistory(e) {
 }
 
 function openGnbSearchHistory() {
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory)
   }
   gnbSearchHistory.classList.add('is-active')
 }
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deleteAllSearchHistoryItems() {
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+deleteAllBtn.addEventListener('click', deleteAllSearchHistoryItems)
