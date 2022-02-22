1492171843268f66521f45135329d1ca99626796
iam-eugene <mai77eugene@gmail.com>
2022-02-23 00:40:23 +0900

    💫 Scroll to tab-panel when product-tab-item is clicked

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,8 +1,10 @@
 const productTab = document.querySelector('.product-tab')
 const productTabBtnList = productTab.querySelectorAll('button')
 
-let currentActiveTab = productTab.querySelector('.is-active')
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
 
+let currentActiveTab = productTab.querySelector('.is-active')
 function toggleActiveTab() {
   const tabItem = this.parentNode
   if (currentActiveTab != tabItem) {
@@ -11,6 +13,22 @@ function toggleActiveTab() {
     currentActiveTab = tabItem
   }
 }
+
+function scrollToTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledBy')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth',
+  })
+}
+
 productTabBtnList.forEach((btn) => {
   btn.addEventListener('click', toggleActiveTab)
+  btn.addEventListener('click', scrollToTabPanel)
 })
