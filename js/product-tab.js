4c06b148ad9c47704e6cedcf1969676f28653499
iam-eugene <mai77eugene@gmail.com>
2022-02-25 00:45:44 +0900

    🐛 Fix bugs on scroll event handler

--- js/product-tab.js
+++ js/product-tab.js
@@ -5,12 +5,19 @@ const TOP_HEADER_DESKTOP = 80 + 50 + 54
 const TOP_HEADER_MOBILE = 50 + 40 + 40
 
 let currentActiveTab = productTab.querySelector('.is-active')
+let disableUpdating = false
+
 function toggleActiveTab() {
   const tabItem = this.parentNode
   if (currentActiveTab != tabItem) {
+    disableUpdating = true
     tabItem.classList.add('is-active')
     currentActiveTab.classList.remove('is-active')
     currentActiveTab = tabItem
+
+    setTimeout(() => {
+      disableUpdating = false
+    }, 1000)
   }
 }
 
@@ -53,9 +60,14 @@ function detectTabPanelPosition() {
     const position = window.scrollY + panel.getBoundingClientRect().top
     productTabPanelPositionMap[id] = position
   })
+  updateActiveTabOnScroll()
 }
 
 function updateActiveTabOnScroll() {
+  if (disableUpdating) {
+    return
+  }
+
   const scrolledAmount =
     window.scrollY +
     (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP + 80 : TOP_HEADER_MOBILE + 8)
@@ -73,12 +85,20 @@ function updateActiveTabOnScroll() {
     newActiveTab = productTabBtnList[0]
   }
 
+  const bodyHeight =
+    document.body.offsetHeight + (window.innerWidth < 1200 ? 56 : 0)
+  if (window.scrollY + window.innerHeight == bodyHeight) {
+    newActiveTab = productTabBtnList[4]
+  }
+
   if (newActiveTab) {
     newActiveTab = newActiveTab.parentNode
 
     if (newActiveTab != currentActiveTab) {
       newActiveTab.classList.add('is-active')
-      currentActiveTab.classList.remove('is-active')
+      if (currentActiveTab != null) {
+        currentActiveTab.classList.remove('is-active')
+      }
       currentActiveTab = newActiveTab
     }
   }
